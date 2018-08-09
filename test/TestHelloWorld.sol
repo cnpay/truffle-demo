81a7beeabd34cee1e0f376134e01e74606dfa40b
liunix <liuyifan.com@gmail.com>
2018-08-09 16:08:04 +0800

    update test

--- test/TestHelloWorld.sol
+++ test/TestHelloWorld.sol
@@ -6,11 +6,11 @@ import "../contracts/HelloWorld.sol";
 
 contract TestHelloWorld {
 
-  function testGreet() public{
+    function testGreet() public{
     // HelloWorld hello = HelloWorld(DeployedAddresses.HelloWorld());
-    HelloWorld hello = new HelloWorld(); //这种方式测试比较简单
-    string memory result = "hello";
-    Assert.equal(hello.greet(result),result,"输入输出应该相等");
-  }
+        HelloWorld hello = new HelloWorld(); //这种方式测试比较简单
+        string memory result = "hello";
+        Assert.equal(hello.greet(result),result,"输入输出应该相等");
+    }
 
 }

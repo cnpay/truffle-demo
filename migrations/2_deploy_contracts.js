41511ba178709006787591d86e27ab7ba5068871
liunix <liuyifan.com@gmail.com>
2018-08-09 16:23:38 +0800

    cp assert.sol to project,add success and failure case show

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
+var Assert = artifacts.require("./Assert.sol");
 var HelloWorld = artifacts.require("./HelloWorld.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(Assert);
+  deployer.link(Assert,HelloWorld)
   deployer.deploy(HelloWorld);
 };
--- test/TestHelloWorld.sol
+++ test/TestHelloWorld.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.2;
 
-import "truffle/Assert.sol";
+import "../contracts/Assert.sol";
 import "../contracts/HelloWorld.sol";
 
 contract TestHelloWorld {
@@ -10,6 +10,12 @@ contract TestHelloWorld {
         HelloWorld hello = new HelloWorld(); //这种方式测试比较简单
         string memory result = "hello";
         Assert.equal(hello.greet(result),result,"输入输出应该相等");
+    }
+
+    function testGreet2() public{
+        HelloWorld hello = new HelloWorld();
+        string memory test = "nidaye";
+        Assert.equal(hello.greet(test),"nidaye2","这个输入输出不相等");
     }
 
 }
